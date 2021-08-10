9c2dc465cad6357c68da3763f468ec90d47224da
John Huelsenbeck <johnh@berkeley.edu>
2021-08-09 18:29:42 -0700

    Starting to implement marginal likelihood calculations.

--- Indelly/Mcmc.hpp
+++ Indelly/Mcmc.hpp
@@ -24,6 +24,8 @@ class Mcmc {
         double          safeExponentiation(double lnX);
         void            sample(int gen, double lnL, double lnP);
         std::string     formattedTime(std::chrono::high_resolution_clock::time_point& t1, std::chrono::high_resolution_clock::time_point& t2);
+        void            runPathSampling(void);
+        void            runPosterior(void);
         Model*          modelPtr;
         RandomVariable* rv;
         int             numMcmcCycles;
