fd657429ae71c364b336581235e9e234a4407456
John Huelsenbeck <johnh@berkeley.edu>
2022-02-03 16:58:31 -0800

    Added files for doing matrix math. Also, added temporary matrix class to be replaced by Container class later.

---
+++ Indelly/MatrixMath.hpp
@@ -0,0 +1,20 @@
+#ifndef MatrixMath_hpp
+#define MatrixMath_hpp
+
+#include "JphMatrix.hpp"
+
+namespace  MatrixMath {
+
+    void    addTwoMatrices(DoubleMatrix* A, DoubleMatrix* B, DoubleMatrix* Res);
+    void    backSubstitutionRow(DoubleMatrix* U, double* b);
+    void    computeLandU(DoubleMatrix* A, DoubleMatrix* L, DoubleMatrix* U);
+    void    divideMatrixByPowerOfTwo(DoubleMatrix* M, int power);
+    void    forwardSubstitutionRow(DoubleMatrix* L, double* b);
+    void    gaussianElimination(DoubleMatrix* A, DoubleMatrix* B, DoubleMatrix* X, DoubleMatrix* L, DoubleMatrix* U, double* b);
+    void    multiplicationByScalar(DoubleMatrix* M, double c);
+    void    multiplicationByScalar(DoubleMatrix* M, double c, DoubleMatrix* Res);
+    void    setIdentity(DoubleMatrix* M);
+    void    multiplyTwoMatrices(DoubleMatrix* A, DoubleMatrix* B, DoubleMatrix* C, DoubleMatrix* temp);
+};
+
+#endif /* Matrix_hpp */
