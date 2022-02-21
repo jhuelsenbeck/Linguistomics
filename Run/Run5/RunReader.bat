dbe1f81b92e456530c4dec7d39593ecdfc51f9cf
Shawn McCreight <shawn.mccreight@gmail.com>
2022-02-21 11:56:14 -0800

    New data, new "tree comparison" appendix.

---
+++ Run/Run5/RunReader.bat
@@ -0,0 +1,1 @@
+"../../IndellyReader/x64/Release/indellyreader.exe" -i "C:/Users/shawn/Documents/Code/Linguistomics/Run/Run5/rwin" -o "C:/Users/shawn/Documents/Code/Linguistomics/Run/Run5/rwin/out" 
