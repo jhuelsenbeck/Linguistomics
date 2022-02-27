4421c54db697e3e198bb13a7dda352c9e2eef360
Shawn McCreight <shawn.mccreight@gmail.com>
2022-02-26 16:35:49 -0800

    New run with curated word set

---
+++ Run/Run6/RunAll.bat
@@ -0,0 +1,3 @@
+"../../Indelly/x64/Release/tonguetwister.exe" -d "config.json" -o "C:/Users/shawn/Documents/Code/Linguistomics/Run/Run6/rwin/out" 
+pause
+"../../IndellyReader/x64/Release/indellyreader.exe" -i "C:/Users/shawn/Documents/Code/Linguistomics/Run/Run6/rwin" -o "C:/Users/shawn/Documents/Code/Linguistomics/Run/Run6/rwin/out" 
---
+++ Run/Run6/RunReader.bat
@@ -0,0 +1,1 @@
+"../../IndellyReader/x64/Release/indellyreader.exe" -i "C:/Users/shawn/Documents/Code/Linguistomics/Run/Run6/rwin" -o "C:/Users/shawn/Documents/Code/Linguistomics/Run/Run6/rwin/out" 
