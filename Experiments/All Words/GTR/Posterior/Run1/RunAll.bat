0a3b6d3c9a05ae511f10cf237d691b65181d8df3
Shawn McCreight <shawn.mccreight@gmail.com>
2022-03-14 18:44:19 -0700

    New Folder structure

---
+++ Experiments/All Words/GTR/Posterior/Run1/RunAll.bat
@@ -0,0 +1,4 @@
+"C:/Users/shawn/Documents/Code/Linguistomics/Indelly/x64/release/tonguetwister.exe" -d "config.json" -o "C:/Users/shawn/Documents/Code/Linguistomics/Experiments/All Words/GTR/Posterior/Run1/Execute/out" 
+pause
+"C:/Users/shawn/Documents/Code/Linguistomics/IndellyReader/x64/release/indellyreader.exe" -i "C:/Users/shawn/Documents/Code/Linguistomics/Experiments/All Words/GTR/Posterior/Run1/Execute" -o "C:/Users/shawn/Documents/Code/Linguistomics/Experiments/All Words/GTR/Posterior/Run1/Execute/out" 
+pause
