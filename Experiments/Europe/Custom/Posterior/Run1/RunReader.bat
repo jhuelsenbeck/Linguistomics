ad148453fa2593b4eeb65d208c6ea39d187a4acc
Shawn McCreight <shawn.mccreight@gmail.com>
2022-03-22 14:28:51 -0700

    Rewrote final versions of European and EuropeBrazil folders

---
+++ Experiments/Europe/Custom/Posterior/Run1/RunReader.bat
@@ -0,0 +1,2 @@
+"C:/Users/shawn/Documents/Code/Linguistomics/IndellyReader/x64/release/indellyreader.exe" -i "C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/Custom/Posterior/Run1/Execute" -o "C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/Custom/Posterior/Run1/Execute/out" 
+pause
