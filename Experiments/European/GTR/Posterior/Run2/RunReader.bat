bf1daecdeac8bd9a28b6b0c081e5c714e3a3d25d
Shawn McCreight <shawn.mccreight@gmail.com>
2022-03-22 14:23:23 -0700

    Removed old folder

--- Experiments/European/GTR/Posterior/Run2/RunReader.bat
+++
@@ -1,2 +0,0 @@
-"C:/Users/shawn/Documents/Code/Linguistomics/IndellyReader/x64/release/indellyreader.exe" -i "C:/Users/shawn/Documents/Code/Linguistomics/Experiments/All Words/GTR/Posterior/Run2/Execute" -o "C:/Users/shawn/Documents/Code/Linguistomics/Experiments/All Words/GTR/Posterior/Run2/Execute/out" 
-pause
