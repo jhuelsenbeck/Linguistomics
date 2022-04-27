3504b1a6c05ec3a6163805c3bfd8ce53e31b2fe9
Shawn McCreight <shawn.mccreight@gmail.com>
2022-04-27 12:33:56 -0700

    Removed old experiments

--- Experiments/Europe/Custom/Marginal/Run2/WinRunReader.bat
+++
@@ -1,2 +0,0 @@
-C:/Users/shawn/Documents/Code/Linguistomics/IndellyReader/x64/release/indellyreader.exe -i C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/Custom/Marginal/Run2/Execute -o C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/Custom/Marginal/Run2/Execute/out
-pause
--- Experiments/Europe/GTR/Marginal/Run1/WinRunReader.bat
+++
@@ -1,2 +0,0 @@
-C:/Users/shawn/Documents/Code/Linguistomics/IndellyReader/x64/release/indellyreader.exe -i C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/GTR/Marginal/Run1/Execute -o C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/GTR/Marginal/Run1/Execute/out
-pause
