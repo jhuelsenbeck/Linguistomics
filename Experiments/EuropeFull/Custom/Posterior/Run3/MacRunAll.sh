3b737d14b17a81b7e105a8004ef2fb1994344592
Shawn McCreight <shawn.mccreight@gmail.com>
2022-07-29 18:32:20 -0700

    Updates to code and data

---
+++ Experiments/EuropeFull/Custom/Posterior/Run3/MacRunAll.sh
@@ -0,0 +1,4 @@
+../../../../../TongueTwister/tonguetwister -d config.json -o Execute/out
+read -rsp $'Press enter to continue...\n'
+../../../../../TongueTwisterReader/ttread -i Execute -o Execute/out
+read -rsp $'Press enter to continue...\n'
---
+++ Experiments/EuropeFull/Custom/Posterior/Run3/MacRunReader.sh
@@ -0,0 +1,2 @@
+../../../../../TongueTwisterReader/ttread -i Execute -o Execute/out
+read -rsp $'Press enter to continue...\n'
