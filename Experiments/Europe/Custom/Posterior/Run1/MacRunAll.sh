8609a11bc7009325f859b452c5a225ef4c4dc7cb
Shawn McCreight <shawn.mccreight@gmail.com>
2022-11-18 11:17:42 -0800

    New run

--- Experiments/Europe/Custom/Posterior/Run1/MacRunAll.sh
+++ Experiments/Europe/Custom/Posterior/Run1/MacRunAll.sh
@@ -1,3 +1,3 @@
-﻿../../../../../TongueTwister/tonguetwister -d config.json -o Execute/out
-../../../../../TongueTwisterReader/ttread -i Execute -b 0 -o Execute/out
+../../../../../TongueTwister/tonguetwister -d config.json -o Execute/out
+../../../../../TongueTwisterReader/ttread -i Execute -b 100 -o Execute/out
 read -rsp $'Press enter to continue...\n'
