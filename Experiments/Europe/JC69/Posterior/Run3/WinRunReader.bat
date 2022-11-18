ecf0f72f2ede41a2383e7597af733483eb1dbc3f
Shawn McCreight <shawn.mccreight@gmail.com>
2022-11-17 19:41:35 -0800

    New experiment data files

--- Experiments/Europe/JC69/Posterior/Run3/WinRunReader.bat
+++ Experiments/Europe/JC69/Posterior/Run3/WinRunReader.bat
@@ -1,2 +1,2 @@
-﻿C:/Users/shawn/Documents/Code/Linguistomics/TongueTwisterReader/x64/release/tonguetwisterreader.exe -i C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/JC69/Posterior/Run3/Execute -b 15000 -o C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/JC69/Posterior/Run3/Execute/out
+﻿C:/Users/shawn/Documents/Code/Linguistomics/TongueTwisterReader/x64/release/tonguetwisterreader.exe -i C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/JC69/Posterior/Run3/Execute -b 0 -o C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/JC69/Posterior/Run3/Execute/out
 pause
