8ec70ab8385b85cd326c3128161d287aa034e15d
Shawn McCreight <shawn.mccreight@gmail.com>
2022-11-18 17:11:57 -0800

    New configurations with parameter file

--- Experiments/Europe/Custom/Posterior/Run1/WinRunReader.bat
+++ Experiments/Europe/Custom/Posterior/Run1/WinRunReader.bat
@@ -1,2 +1,2 @@
-﻿C:/Users/shawn/Documents/Code/Linguistomics/TongueTwisterReader/x64/release/tonguetwisterreader.exe -i C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/Custom/Posterior/Run1/Execute -b 0 -o C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/Custom/Posterior/Run1/Execute/out
+﻿C:/Users/shawn/Documents/Code/Linguistomics/TongueTwisterReader/x64/release/tonguetwisterreader.exe -i C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/Custom/Posterior/Run1/Execute -b 15000 -o C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Europe/Custom/Posterior/Run1/Execute/out
 pause
