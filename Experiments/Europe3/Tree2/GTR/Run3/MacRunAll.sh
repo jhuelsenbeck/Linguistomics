2d35e56ad179d8495bb9380f8061f7f0ed1b252a
Shawn McCreight <shawn.mccreight@gmail.com>
2023-04-21 20:11:53 -0700

    The rest of the batch files

--- Experiments/Europe3/Tree2/GTR/Run3/MacRunAll.sh
+++ Experiments/Europe3/Tree2/GTR/Run3/MacRunAll.sh
@@ -1,3 +1,3 @@
-﻿../../../../../TongueTwister/tonguetwister -d config.json -o Execute/out
+../../../../../TongueTwister/tonguetwister -d config.json -o Execute/out
 ../../../../../TongueTwisterReader/ttread -i Execute -b 15000 -o Execute/out
 read -rsp $'Press enter to continue...\n'
